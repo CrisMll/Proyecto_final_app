c9b2bbde754880a47579cfe93c2ba542e35363b0
CrisMLL <crismll.dev@gmail.com>
2024-04-28 19:07:45 +0200

    reorganización estructuras del codigo

--- app.py
+++ app.py
@@ -36,18 +36,6 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-
-@login_manager_app.user_loader
-def load_user(id_usuario):
-    response = supabase.table('usuarios').select('*').eq('id', id_usuario).execute()
-    user_data = response.data
-    if user_data:
-        user_data = user_data[0]
-        user = User(id_usuario=user_data['id'], name=user_data['name'], email=user_data['email'], 
-                    passwrd=user_data['passwrd'], role=user_data['id_rol'], favorite_recipes=user_data['favorite_recipes'])
-        return user
-    return None
-
 ###------------- RUTAS -----------------###
 
 #? RUTA HOME Y PRINCIPALES DE RECETAS
@@ -78,33 +66,22 @@ def get_recipe(id):
     return render_template("recipe.html", receta=receta)
 
 
-#? RUTA DE RECETAS FAVORITAS
-
-@app.route('/add_favorite/<int:recipe_id>', methods=['POST'])
-@login_required
-def add_favorite(recipe_id):
-    response = supabase.table("recetas").select("nombre_receta").eq("id_receta", recipe_id).execute()
-    receta = response.data[0]
-    nombre_receta = receta['nombre_receta']
-    current_user.add_favorite_recipe(nombre_receta)
-    current_user.favorite_recipes = current_user.get_favorite_recipes()
-    flash('Receta añadida', 'added')
-    return redirect(url_for('profile', message='added'))
-
-@app.route('/remove_favorite/<int:recipe_id>', methods=['POST'])
-@login_required
-def remove_favorite(recipe_id):
-    response = supabase.table("recetas").select("nombre_receta").eq("id_receta", recipe_id).execute()
-    receta = response.data[0]
-    nombre_receta = receta['nombre_receta']
-    current_user.remove_favorite_recipe(nombre_receta)
-    flash('Receta eliminada', 'removed')
-    return redirect(url_for('profile', message='removed'))
-
-
 
 #? CONTROL DE USUARIOS
 
+#datos de usuario
+@login_manager_app.user_loader
+def load_user(id_usuario):
+    response = supabase.table('usuarios').select('*').eq('id', id_usuario).execute()
+    user_data = response.data
+    if user_data:
+        user_data = user_data[0]
+        user = User(id_usuario=user_data['id'], name=user_data['name'], email=user_data['email'], 
+                    passwrd=user_data['passwrd'], role=user_data['id_rol'], favorite_recipes=user_data['favorite_recipes'])
+        return user
+    return None
+
+#login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     show_signup_link = False
@@ -120,6 +97,7 @@ def login():
             show_signup_link = True  
     return render_template('login.html', show_signup_link=show_signup_link)
 
+#signup
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
@@ -131,11 +109,13 @@ def signup():
 
         if passwrd != confirm_passwrd:
             flash('Las contraseñas no coinciden.', 'error')
+            return redirect(url_for('signup'))
         else:
             response = supabase.table('usuarios').select('*').eq('email', email).execute()
             user_exists = response.data
             if user_exists:
                 flash('El usuario ya existe.', 'error')
+                return redirect(url_for('signup'))
             else:
                 hashed_password = generate_password_hash(passwrd)
                 response = supabase.from_('usuarios').insert({'name': name, 'email': email, 'passwrd': hashed_password}).execute()
@@ -144,40 +124,14 @@ def signup():
 
     return render_template('signup.html', form=form)
 
-
-
-
-
-'''@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        passwrd = request.form['passwrd']
-        confirm_passwrd = request.form['confirm_passwrd']
-        
-        if passwrd != confirm_passwrd:
-            flash('Las contraseñas no coinciden.', 'error')
-        else:
-            response = supabase.table('usuarios').select('*').eq('email', email).execute()
-            user_exists = response.data
-            if user_exists:
-                flash('El usuario ya existe.', 'error')
-                
-            else:
-                hashed_passwrd = generate_password_hash(passwrd)
-                response = supabase.from_('usuarios').insert({'name': name, 'email': email, 'passwrd': hashed_passwrd}).execute()
-                flash('Registrado con éxito. Por favor, inicia sesión.', 'success')
-                return redirect(url_for('login'))
-            
-    return render_template('signup.html')'''
-
+#logout
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#mostrar user profile
 @app.route('/profile')
 @login_required
 def profile():
@@ -188,37 +142,70 @@ def profile():
 @app.route('/admin', methods=['GET', 'POST'])
 @login_required
 def admin():
-    if current_user.role != 1:
+    if current_user.role == 1:
+        response = supabase.table('secciones').select('id_seccion').execute()
+        secciones = response.data
+        response = supabase.table('ingredientes').select('id_ingrediente', 'nombre_ingrediente').execute()
+        ingredientes = response.data
+        return render_template('admin.html', secciones=secciones, ingredientes=ingredientes)
+    else:
         return redirect(url_for('home'))
-    if request.method == 'POST':
-        nombre_receta = request.form['nombre_receta']
-        descripcion = request.form['descripcion']
-        tipo_seccion_id = request.form['tipo_seccion_id']
-        preparacion = request.form['preparacion']
-        ingredientes = request.form.getlist('ingredientes')
-        imagen_receta = request.files['imagen_receta']
-        if imagen_receta and allowed_file(imagen_receta.filename):
-            filename = secure_filename(imagen_receta.filename)
-            imagen_receta.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            response = supabase.table('recetas').insert({
-                'nombre_receta': nombre_receta,
-                'imagen_receta': filename,
-                'descripcion': descripcion,
-                'tipo_seccion_id': tipo_seccion_id,
-                'preparacion': preparacion,
-                'ingredientes': ingredientes
-            }).execute()
-            if response.get('error'):
-                flash('Error al añadir la receta.', 'error')
-            else:
-                flash('Receta añadida', 'success')
+@app.route('/admin/new', methods=['POST'])
+def admin_recipe():    
+    if current_user.role == 1:
+        if request.method == 'POST':
+            nombre_receta = request.form['nombre_receta']
+            descripcion = request.form['descripcion']
+            tipo_seccion_id = request.form['tipo_seccion_id']
+            preparacion = request.form['preparacion']
+            ingredientes = request.form.getlist('ingredientes')
+            imagen_receta = request.files['imagen_receta']
+            
+            if imagen_receta and allowed_file(imagen_receta.filename):
+                filename = secure_filename(imagen_receta.filename)
+                imagen_receta.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-    response = supabase.table('secciones').select('id_seccion').execute()
-    secciones = response.data
-    response = supabase.table('ingredientes').select('id_ingrediente').execute()
-    ingredientes = response.data
-    return render_template('admin.html', secciones=secciones, ingredientes=ingredientes)
+                response = supabase.table('recetas').insert({
+                    'nombre_receta': nombre_receta,
+                    'imagen_receta': filename,
+                    'descripcion': descripcion,
+                    'tipo_seccion_id': tipo_seccion_id,
+                    'preparacion': preparacion,
+                    'ingredientes': ingredientes
+                }).execute()
+                if response.get('error'):
+                    flash('Error al añadir la receta.', 'error')
+                else:
+                    flash('Receta añadida', 'success')
+    else:
+        return redirect(url_for('home'))
+
+
+#? RUTA DE RECETAS FAVORITAS
+
+#add favorita
+@app.route('/add_favorite/<int:recipe_id>', methods=['POST'])
+@login_required
+def add_favorite(recipe_id):
+    response = supabase.table("recetas").select("nombre_receta").eq("id_receta", recipe_id).execute()
+    receta = response.data[0]
+    nombre_receta = receta['nombre_receta']
+    current_user.add_favorite_recipe(nombre_receta)
+    current_user.favorite_recipes = current_user.get_favorite_recipes()
+    flash('Receta añadida', 'added')
+    return redirect(url_for('profile', message='added'))
+
+#remove favorita
+@app.route('/remove_favorite/<int:recipe_id>', methods=['POST'])
+@login_required
+def remove_favorite(recipe_id):
+    response = supabase.table("recetas").select("nombre_receta").eq("id_receta", recipe_id).execute()
+    receta = response.data[0]
+    nombre_receta = receta['nombre_receta']
+    current_user.remove_favorite_recipe(nombre_receta)
+    flash('Receta eliminada', 'removed')
+    return redirect(url_for('profile', message='removed'))
 
 
 
@@ -227,8 +214,6 @@ def admin():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact_page():
     return render_template('contact.html')
-
-
 
 
 if __name__ == '__main__':
--- models/forms.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Recuérdame')
-    submit = SubmitField('Login')
