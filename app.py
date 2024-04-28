45dfe666c2308c27afa34ee01c9a08654e9da4d0
CrisMLL <crismll.dev@gmail.com>
2024-04-28 14:16:17 +0200

    Arreglo y manejo de errores en funciones de inicio sesión y registro. Añado vista admin

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages, send_from_directory
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
+from supabase import create_client
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_wtf.csrf import CSRFProtect
 from werkzeug.security import check_password_hash, generate_password_hash
-from supabase import create_client
+from werkzeug.utils import secure_filename
+import uuid
 
 #Models
 from models.modelUser import ModelUser
@@ -27,6 +29,12 @@ url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
 app.secret_key = os.getenv('SECRET_KEY')
+app.config['UPLOAD_FOLDER'] = 'static/img/img_recetas'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
 
 @login_manager_app.user_loader
 def load_user(id_usuario):
@@ -109,26 +117,31 @@ def login():
         else:
             flash('Datos incorrectos. Por favor, inténtalo de nuevo.', 'error')
             show_signup_link = True  
-    return render_template('auth/login.html', show_signup_link=show_signup_link)
+    return render_template('login.html', show_signup_link=show_signup_link)
 
 @app.route('/signup', methods=['GET', 'POST'])
-def register():
+def signup():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         passwrd = request.form['passwrd']
         confirm_passwrd = request.form['confirm_passwrd']
+        
         if passwrd != confirm_passwrd:
             flash('Las contraseñas no coinciden.', 'error')
         else:
-            hashed_passwrd = generate_password_hash(passwrd)
-            response = supabase.from_('usuarios').insert({'name': name, 'email': email, 'passwrd': hashed_passwrd}).execute()
-            if response.get('error'):
+            response = supabase.table('usuarios').select('*').eq('email', email).execute()
+            user_exists = response.data
+            if user_exists:
                 flash('El usuario ya existe.', 'error')
+                
             else:
+                hashed_passwrd = generate_password_hash(passwrd)
+                response = supabase.from_('usuarios').insert({'name': name, 'email': email, 'passwrd': hashed_passwrd}).execute()
                 flash('Registrado con éxito. Por favor, inicia sesión.', 'success')
                 return redirect(url_for('login'))
-    return render_template('auth/signup.html')
+            
+    return render_template('signup.html')
 
 @app.route('/logout')
 @login_required
@@ -141,6 +154,50 @@ def logout():
 def profile():
     favorite_recipes = current_user.get_favorite_recipes()
     return render_template('profile.html', favorite_recipes=favorite_recipes)
+
+#? ROL ADMIN
+@app.route('/admin', methods=['GET', 'POST'])
+@login_required
+def admin():
+    if current_user.role != 1:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        nombre_receta = request.form['nombre_receta']
+        descripcion = request.form['descripcion']
+        tipo_seccion_id = request.form['tipo_seccion_id']
+        preparacion = request.form['preparacion']
+        ingredientes = request.form.getlist('ingredientes')
+        imagen_receta = request.files['imagen_receta']
+        if imagen_receta and allowed_file(imagen_receta.filename):
+            filename = secure_filename(imagen_receta.filename)
+            imagen_receta.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            response = supabase.table('recetas').insert({
+                'nombre_receta': nombre_receta,
+                'imagen_receta': filename,
+                'descripcion': descripcion,
+                'tipo_seccion_id': tipo_seccion_id,
+                'preparacion': preparacion,
+                'ingredientes': ingredientes
+            }).execute()
+            if response.get('error'):
+                flash('Error al añadir la receta.', 'error')
+            else:
+                flash('Receta añadida', 'success')
+
+    response = supabase.table('secciones').select('id_seccion').execute()
+    secciones = response.data
+    response = supabase.table('ingredientes').select('id_ingrediente').execute()
+    ingredientes = response.data
+    return render_template('admin.html', secciones=secciones, ingredientes=ingredientes)
+
+
+
+
+
+
+
+
 
 
 #? CONTACTO
