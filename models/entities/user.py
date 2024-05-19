4b223efd995d0be0adddbb854b6f39efc2118ae5
Cris Morales LL <125212295+CrisMll@users.noreply.github.com>
2024-05-19 10:32:11 +0200

    Add files via upload
    Uploading files to main branch

---
+++ models/entities/user.py
@@ -0,0 +1,93 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from supabase import create_client, Client
+from flask_login import UserMixin
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+
+class User(UserMixin):
+    def __init__(self, id_usuario, name, email, passwrd, role, favorite_recipes=None):
+        self.id_usuario = id_usuario
+        self.name = name
+        self.email = email
+        self.passwrd = passwrd
+        self.role = role
+        self.favorite_recipes = favorite_recipes if favorite_recipes else []
+        
+    
+    @classmethod
+    def load_user(cls, id_usuario):
+        response = supabase.table('usuarios').select('*').eq('id', id_usuario).execute()
+        user_data = response.data
+        if user_data:
+            user_data = user_data[0]
+            user = User(
+                id_usuario=user_data['id'],
+                name=user_data['name'],
+                email=user_data['email'], 
+                passwrd=user_data['passwrd'],
+                role=user_data['id_rol'],
+                favorite_recipes=user_data['favorite_recipes']
+            )
+            return user
+        return None
+        
+    def get_id(self):
+        return str(self.id_usuario)
+
+    @classmethod    
+    def get_by_id(cls, id_usuario):
+        response = supabase.table('usuarios').select('*').eq('id', id_usuario).execute()
+        user_data = response.data
+        if user_data:
+            user_data = user_data[0]
+            return cls(user_data)
+        return None
+
+    @classmethod
+    def get_by_email(cls, email):
+        response = supabase.table('usuarios').select('*').eq('email', email).execute()
+        user_data = response.data
+        if user_data:
+            user_data = user_data[0]
+            return cls(id_usuario=user_data['id'], name=user_data['name'], email=user_data['email'], 
+                    passwrd=user_data['passwrd'], role=user_data['id_rol'])
+        return None    
+    
+    @classmethod
+    def hash_password(cls, passwrd):
+        return generate_password_hash(passwrd)
+        
+    @classmethod
+    def check_password(cls, hashed_password, passwrd):
+        return check_password_hash(hashed_password, passwrd)
+
+
+    #? FUNCIONES DE RECETAS FAVORITAS
+    
+    def add_favorite_recipe(self, recipe_name):
+        response = supabase.table('usuarios').select('favorite_recipes').eq('id', self.id_usuario).execute()
+        current_favorites = response.data[0]['favorite_recipes']
+        if current_favorites is None:
+            current_favorites = []
+        if recipe_name not in current_favorites:
+            current_favorites.append(recipe_name)
+        supabase.table('usuarios').update({'favorite_recipes': current_favorites}).eq('id', self.id_usuario).execute()
+        
+    def get_favorite_recipes(self):
+        return self.favorite_recipes    
+        
+    def remove_favorite_recipe(self, recipe_name):
+        response = supabase.table('usuarios').select('favorite_recipes').eq('id', self.id_usuario).execute()
+        current_favorites = response.data[0]['favorite_recipes']
+        if recipe_name in current_favorites:
+            current_favorites.remove(recipe_name)
+        supabase.table('usuarios').update({'favorite_recipes': current_favorites}).eq('id', self.id_usuario).execute()
+        
+
+        
---
+++ models/modelUser.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
+from .entities.user import User
+
+load_dotenv()
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+class ModelUser():
+    
+    @classmethod
+    def login(self, supabase, user):
+        try:
+            supabase = supabase.table('usuarios').select('id, email, passwrd, name').filter('email', 'eq', user.email).execute()
+            account = supabase.data
+            if account is not None and len(account) > 0:
+                info = account[0]
+                user=User(info['id'], info['email'], User.check_password(info['passwrd'],user.passwrd),info['name'])
+                return user
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
+    
+    @classmethod
+    def get_by_id(self, supabase, id_usuario):
+        try:
+            supabase = supabase.table('usuarios').select('id, email, name').filter('id', 'eq', id_usuario).execute()
+            account = supabase.data
+            if account != None and len(account) > 0:
+                info = account[0]
+                logged_user=User(info['id'], info['email'], None, info['name'])
+                return logged_user
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
+        
+        
+    @classmethod
+    def check_username_exists(cls, supabase, username):
+        try:
+            existing_user = supabase.table('usuarios').select('email').filter('email', 'eq', username).execute()
+            return existing_user.data is not None and len(existing_user.data) > 0
+        except Exception as ex:
+            raise Exception(ex)
+
+
