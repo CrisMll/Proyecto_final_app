a2a0e3d8f11452026250024da7b8147b56293769
Cris Morales LL <125212295+CrisMll@users.noreply.github.com>
2024-05-20 22:34:01 +0200

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
 
-//? Función para el botón de volver en la sección
+//? Utilidades
+//Función para el botón de volver en la sección
 function goBack() {
     window.history.back();
 }
 
-//? Funcion para copiar el email de la sección de contacto
+// Funcion para copiar el email de la sección de contacto
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
@@ -25,49 +26,26 @@ function copyEmail() {
 
     isClicked = true;
 }
-document.getElementById('copyEmail').addEventListener('click', copyEmail);
-
-document.getElementById('copyEmail').addEventListener('mouseout', function () {
-    if (isClicked) {
-        let button = document.getElementById('copyEmail');
-        let tooltip = bootstrap.Tooltip.getInstance(button);
-        tooltip.hide();
-        button.setAttribute('data-bs-original-title', 'Click para copiar');
-        tooltip = new bootstrap.Tooltip(button);
-
-        isClicked = false;
-    }
-});
 
 
-//? Manejo del interruptor del modo oscuro permanente en la sesion
+const copyEmailButton = document.getElementById('copyEmail');
 
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark');
-    
-    if (body.classList.contains('dark')) {
-        localStorage.setItem('darkMode', 'enabled');
-    } else {
-        localStorage.setItem('darkMode', 'disabled');
-    }
+if (copyEmailButton) {
+    copyEmailButton.addEventListener('click', copyEmail);
+    copyEmailButton.addEventListener('mouseout', function () {
+        if (isClicked) {
+            let button = document.getElementById('copyEmail');
+            let tooltip = bootstrap.Tooltip.getInstance(button);
+            tooltip.hide();
+            button.setAttribute('data-bs-original-title', 'Click para copiar');
+            tooltip = new bootstrap.Tooltip(button);
+
+            isClicked = false;
+        }
+    });
 }
 
-//? Cargar el estado del modo oscuro cuando se carga la página
-document.addEventListener('DOMContentLoaded', (event) => {
-    const darkMode = localStorage.getItem('darkMode');
-    if (darkMode === 'enabled') {
-        document.body.classList.add('dark');
-        document.getElementById('dark-mode-toggle').checked = true;
-    } else {
-        document.body.classList.remove('dark');
-        document.getElementById('dark-mode-toggle').checked = false;
-    }
-});
-
-
-
-//? Boton volver arriba
+// Boton volver arriba
 
 document.addEventListener("DOMContentLoaded", function() {
     const btn = document.getElementById("back-to-top");
@@ -87,4 +65,71 @@ document.addEventListener("DOMContentLoaded", function() {
             behavior: 'smooth' 
         });
     });
+});
+
+//? Funciones de cookies modo oscuro
+
+// Manejo del interruptor del modo oscuro permanente en la sesion, modo large y modo movil
+function setCookie(cookieName, cookieValue, daysToExpire) {
+    const currentDate = new Date();
+    currentDate.setTime(currentDate.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + currentDate.toUTCString();
+    document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
+}
+
+function getCookie(cookieName) {
+    const nameEquals = cookieName + "=";
+    const allCookies = document.cookie.split(';');
+    for (let cookie of allCookies) {
+        cookie = cookie.trim();
+        if (cookie.startsWith(nameEquals)) {
+            return cookie.substring(nameEquals.length);
+        }
+    }
+    return null;
+}
+
+function deleteCookie(cookieName) {
+    document.cookie = `${cookieName}=; Max-Age=-99999999;path=/`;
+}
+
+function applyDarkMode() {
+    document.body.classList.add('dark');
+    document.getElementById('darkModeToggleLg').checked = true;
+    document.getElementById('darkModeToggleSm').checked = true;
+}
+
+function removeDarkMode() {
+    document.body.classList.remove('dark');
+    document.getElementById('darkModeToggleLg').checked = false;
+    document.getElementById('darkModeToggleSm').checked = false;
+}
+
+function checkDarkMode() {
+    console.log("checkDarkMode function is executing");
+    const darkModeEnabled = getCookie('darkMode');
+    if (darkModeEnabled === 'enabled') {
+        applyDarkMode();
+    } else {
+        removeDarkMode();
+    }
+}
+
+function toggleDarkMode() {
+    console.log("toggleDarkMode function is executing");
+    const darkModeEnabled = getCookie('darkMode');
+    if (darkModeEnabled === 'enabled') {
+        setCookie('darkMode', 'disabled', 7);
+        removeDarkMode();
+    } else {
+        setCookie('darkMode', 'enabled', 7);
+        applyDarkMode();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOMContentLoaded event is firing");
+    document.getElementById('darkModeToggleLg').addEventListener('change', toggleDarkMode);
+    document.getElementById('darkModeToggleSm').addEventListener('change', toggleDarkMode);
+    checkDarkMode();
 });
